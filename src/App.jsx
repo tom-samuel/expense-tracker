7269aa65ba5d38dc49952063e92fe83a811a3ae9
Tomiwa Samuel <Createev69@gmail.com>
2025-11-13 15:54:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [token, setToken] = React.useState(null);
 
   return (
-    <div className="min-h-screen p-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       {!token ? <AuthForm setToken={setToken} /> : <Dashboard token={token} />}
     </div>
   );
